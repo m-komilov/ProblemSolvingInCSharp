e4497ec01b8ea1c4ce3620e2ed6696c06cfbd3ef
m-komilov <muhammadabdulloh.uzinfocom@gmail.com>
2022-06-17 16:00:57 +0500

    Matn fayllar 5 - Masala

--- Program.cs
+++ Program.cs
@@ -8,76 +8,27 @@ namespace TEsting
     {
         static void Main(string[] args)
         {
-            I_S.s_S(123);
+            StringBuilder matnlar = new StringBuilder(
+                "Assalom 1 deganda 2 ochilar 3 deganda 4 qo'shilar hullas 5 ga 6 ni qo'shsak 7 bo'lmas haha"
+                );
+            raqam(matnlar);
+            
 
         }
-    }
 
-    public static class I_S
-    {
-        public static void s_S(int number)
+        static void raqam(StringBuilder matnlar)
         {
-            int number1 = number;
-            int number2 = number;
-            int number3 = number;
+            StringBuilder numberMatnlari = new StringBuilder();
 
-            int quotient;
-            int i = 1, j;
-            char[] hexadecimalNumber = new char[100];
-            char temp1;
-
-            quotient = number1;
-            while (quotient != 0)
+            for (int i = 0; i < matnlar.Length; i++)
             {
-                int temp = quotient % 16;
-                if (temp < 10)
-                    temp = temp + 48;
-                else
-                    temp = temp + 55;
-                temp1 = Convert.ToChar(temp);
-                hexadecimalNumber[i++] = temp1;
-                quotient = quotient / 16;
+                if (matnlar[i] == '0' || matnlar[i] == '1' || matnlar[i] == '2' || matnlar[i] == '3' || matnlar[i] == '4' || matnlar[i] == '5' || matnlar[i] == '6' || matnlar[i] == '7' || matnlar[i] == '8' || matnlar[i] == '9')
+                {
+                    numberMatnlari.Append(matnlar[i].ToString());
+                }
             }
-
-            StringBuilder value = new StringBuilder();
-
-            for (j = i - 1; j > 0; j--)
-                value.Append(hexadecimalNumber[j]);
-
-            Console.Write("16 lik sanoq sistemasida: ");
-            Console.WriteLine(value);
-            //********************************************************************/
-
-            string result;
-
-            result = "";
-            while (number2 > 1)
-            {
-                int remainder = number2 % 2;
-                result = Convert.ToString(remainder) + result;
-                number2 /= 2;
-            }
-            result = Convert.ToString(number2) + result;
-
-            Console.Write("2 lik sanoq sistemasidagi ko'rinishi: ");
-            Console.WriteLine(result);
-
-            //***********************************************************************/
-
-            int i1, j1, octal = 0;
-            i1 = 1;
-
-            for (j1 = number3; j1 > 0; j1 = j1 / 8)
-            {
-                octal = octal + (j1 % 8) * i1;
-                i1 = i1 * 10;
-                number3 = number3 / 8;
-            }
-
-            Console.Write("8 lik sanoq sistemasigagi ko'rinishi: ");
-            Console.WriteLine(octal);
+            Console.WriteLine(numberMatnlari);
         }
-
     }
 
 }
